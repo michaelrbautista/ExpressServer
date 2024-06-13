3b98c75492355dd13e785aa50f6190d84b7ecfd2
michael-bautista <mrbautistaios@gmail.com>
2024-06-12 20:32:06 -0700

    Added create account and account link endpoints

--- routes/stripeAccount.js
+++ routes/stripeAccount.js
@@ -1,9 +1,42 @@
 const express = require('express');
 const router = express.Router();
+router.use(express.json())
 
-router.post("/", (req, res) => {
-    // Get all accounts
-    res.send({ data: "Get all accounts" })
+const stripe = require('stripe')(process.env.STRIPE_TEST_SECRET_KEY)
+
+// Create stripe account
+router.post("/create", async function(req, res) {
+    try {
+        const account = await stripe.accounts.create({
+             type: 'standard',
+             business_profile: {
+                mcc: 5815
+             }
+        });
+
+        res.send(account)
+    } catch (e) {
+        throw new Error(e);
+    }
+})
+
+// Create stripe account link
+router.post("/accountLink", async function (req, res) {
+    try {
+        const accountLink = await stripe.accountLinks.create({
+            account: req.body.stripeAccountId,
+            refresh_url: 'https://michaelrbautista.github.io/stripeurl/',
+            return_url: 'https://michaelrbautista.github.io/stripeurl/',
+            type: 'account_onboarding',
+            collection_options: {
+              fields: 'eventually_due',
+            },
+          });
+
+        res.send(accountLink)
+    } catch (e) {
+        throw new Error(e);
+    }
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,14 +3,12 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 
-const stripe = require('stripe')(process.env.STRIPE_TEST_SECRET_KEY)
-
 const stripeAccountRoute = require('./routes/stripeAccount');
 app.use('/stripeAccount', stripeAccountRoute);
 
 app.listen(
     3000,
-    () => console.log(`Live on ${process.env.SERVER_URL}`)
+    () => console.log(`Live on http://localhost:3000`)
 )
 
 
