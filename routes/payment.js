3ebd745cd1b78c7c363bb3de275192d1102f72cd
michael-bautista <mrbautistaios@gmail.com>
2024-06-16 14:39:27 -0700

    fixed application fee amount again

--- routes/payment.js
+++ routes/payment.js
@@ -13,7 +13,7 @@ router.post("/paymentSheet", async function(req, res) {
         const paymentIntent = await stripe.paymentIntents.create({
             amount: req.body.amount,
             currency: 'usd',
-            application_fee_amount: req.body.amount * 0.05 * 100,
+            application_fee_amount: 200,
             transfer_data: {
               destination: req.body.destinationAccountId,
             },
