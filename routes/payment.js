d6cb2ee96a9b3b05883cf29023fc078fd433bc7b
michael-bautista <mrbautistaios@gmail.com>
2024-06-16 10:39:17 -0700

    added stripe test key

--- routes/payment.js
+++ routes/payment.js
@@ -5,7 +5,7 @@ router.use(express.urlencoded({ extended: true }));
 const bodyParser = require('body-parser');
 router.use(bodyParser.json());
 
-const stripe = require('stripe')(process.env.STRIPE_TEST_SECRET_KEY)
+const stripe = require('stripe')('sk_test_51P3659KRUtiKYn5d3lm7pT9qcMjrIIEVIOI7oeiLeHoo60pQJOxNo9PEjlsMvxBaYLtqXLQSoWZN577CcBw2cO2T00aheP8Me9')
 
 // Create stripe account
 router.post("/paymentSheet", async function(req, res) {
